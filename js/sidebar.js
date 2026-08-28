c4a9d8f59a3d4578aa6d1cf4bdbff231000e7344
Arun Kumar <aasaitechproviders@gmail.com>
2026-08-28 13:45:40 +0530

    fix: remove incorrectly nested tmp folder

--- js/sidebar.js
+++ js/sidebar.js
@@ -16,6 +16,7 @@ const NAV_SECTIONS = [
       { key: 'team',              href: 'team.html',              label: 'Team & Access',      icon: 'M12 15a4 4 0 100-8 4 4 0 000 8z M2 21a10 10 0 0120 0 M8.5 8.5a4 4 0 015.2-.3' },
       { key: 'subscription-plans', href: 'subscription-plans.html', label: 'Delivery Frequency', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2zM9 12h6M9 16h4' },
       { key: 'subscription-durations', href: 'subscription-durations.html', label: 'Subscription Durations', icon: 'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z' },
+      { key: 'pricing-config', href: 'pricing-config.html', label: 'Pricing Config', icon: 'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z' },
       { key: 'procurement',         href: 'procurement.html',         label: 'Procurement',        icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01' },
       { key: 'appointments',      href: 'appointments.html',      label: 'Appointments',       icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
       { key: 'announcements',     href: 'announcements.html',     label: 'Announcements',      icon: 'M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9' },
